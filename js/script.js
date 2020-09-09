dbfdfbe9158b4b6346db8480c8689abae065f661
Micaele Carvalho <micaelecarv@gmail.com>
2020-09-09 18:15:55 +0000

    XXXX

---
+++ js/script.js
@@ -0,0 +1,6 @@
+if(document.getElementById("nome") estiver preenchido && document.getElementById("email") estiver preenchido){
+  envie os dados
+}else{
+  peça a usuária que preencha os campos nome e email
+}
+
